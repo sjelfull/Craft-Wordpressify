a7f347b8e61352288de8f71217258c05e2d377ad
Fred Carlsen <fred@sjelfull.no>
2016-03-30 17:40:21 +0200

    Cleanup + screenshot

--- README.md
+++ README.md
@@ -1,37 +1,23 @@
 # Wordpressify plugin for Craft CMS
 
-Convert Craft into Wordpress
+Transform Craft into Wordpress
 
-![Screenshot](resources/screenshots/plugin_logo.png)
+![Screenshot](resources/screenshots/login-screen.png)
 
 ## Installation
 
 To install Wordpressify, follow these steps:
 
 1. Download & unzip the file and place the `wordpressify` directory into your `craft/plugins` directory
-2.  -OR- do a `git clone https://github.com/sjelfull/wordpressify.git` directly into your `craft/plugins` folder.  You can then update it with `git pull`
+2.  -OR- do a `git clone https://github.com/sjelfull/Craft-Wordpressify.git` directly into your `craft/plugins` folder.  You can then update it with `git pull`
 3. Install plugin in the Craft Control Panel under Settings > Plugins
 4. The plugin folder should be named `wordpressify` for Craft to see it.  GitHub recently started appending `-master` (the branch name) to the name of the folder for zip file downloads.
 
 Wordpressify works on Craft 2.4.x and Craft 2.5.x.
 
-## Wordpressify Overview
-
--Insert text here-
-
-## Configuring Wordpressify
-
--Insert text here-
-
-## Using Wordpressify
-
--Insert text here-
-
 ## Wordpressify Roadmap
 
-Some things to do, and ideas for potential features:
-
-* Release it
+* Take over the world
 
 ## Wordpressify Changelog
 
--- WordpressifyPlugin.php
+++ WordpressifyPlugin.php
@@ -63,7 +63,7 @@ class WordpressifyPlugin extends BasePlugin
      */
     public function getDescription()
     {
-        return Craft::t('Convert Craft into Wordpress');
+        return Craft::t('Transform Craft into Wordpress');
     }
 
     /**
@@ -131,86 +131,4 @@ class WordpressifyPlugin extends BasePlugin
     {
         return 'http://sjelfull.no';
     }
-
-    /**
-     * Returns whether the plugin should get its own tab in the CP header.
-     *
-     * @return bool
-     */
-    public function hasCpSection()
-    {
-        return false;
-    }
-
-    /**
-     * Called right before your plugin’s row gets stored in the plugins database table, and tables have been created
-     * for it based on its records.
-     */
-    public function onBeforeInstall()
-    {
-    }
-
-    /**
-     * Called right after your plugin’s row has been stored in the plugins database table, and tables have been
-     * created for it based on its records.
-     */
-    public function onAfterInstall()
-    {
-    }
-
-    /**
-     * Called right before your plugin’s record-based tables have been deleted, and its row in the plugins table
-     * has been deleted.
-     */
-    public function onBeforeUninstall()
-    {
-    }
-
-    /**
-     * Called right after your plugin’s record-based tables have been deleted, and its row in the plugins table
-     * has been deleted.
-     */
-    public function onAfterUninstall()
-    {
-    }
-
-    /**
-     * Defines the attributes that model your plugin’s available settings.
-     *
-     * @return array
-     */
-    protected function defineSettings()
-    {
-        return array(
-            'someSetting' => array(AttributeType::String, 'label' => 'Some Setting', 'default' => ''),
-        );
-    }
-
-    /**
-     * Returns the HTML that displays your plugin’s settings.
-     *
-     * @return mixed
-     */
-    public function getSettingsHtml()
-    {
-       return craft()->templates->render('wordpressify/Wordpressify_Settings', array(
-           'settings' => $this->getSettings()
-       ));
-    }
-
-    /**
-     * If you need to do any processing on your settings’ post data before they’re saved to the database, you can
-     * do it with the prepSettings() method:
-     *
-     * @param mixed $settings  The Widget's settings
-     *
-     * @return mixed
-     */
-    public function prepSettings($settings)
-    {
-        // Modify $settings here...
-
-        return $settings;
-    }
-
 }
